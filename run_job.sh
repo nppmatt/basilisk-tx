61de66c079e0232ab7db60d4556b881680746862
Matthew Cho <mc462@login01.tartan.njit.edu>
2024-02-01 13:16:37 -0500

    debugging run_job

--- run_job.sh
+++ run_job.sh
@@ -1,16 +1,20 @@
 #!/bin/bash -l
 
 #SBATCH -J basilisk_job
-#SBATCH -o slurm.out
+#SBATCH -o out/slurm.out
 #SBATCH -e slurm.err
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --partition=test
+#SBATCH --partition=general
+
+echo "test"
 
 # Load all needed modules for Basilisk
 module purge > /dev/null 2>&1
 source load_modules.sh
+echo "Modules loaded."
 
 # Run the file passed onto these parameters.
-."$1"
+echo "Passing $1"
+./"$1"
