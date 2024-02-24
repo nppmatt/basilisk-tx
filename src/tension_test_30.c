c68d64c02cc51b22a7867707c55e582846600212
Matthew Cho <mc462@login01.tartan.njit.edu>
2024-02-24 04:39:18 -0500

    trying neumann specification for bottom fraction

--- src/tension_test_30.c
+++ src/tension_test_30.c
@@ -28,7 +28,8 @@ tau_qq[bottom] = dirichlet(0);
 u.t[bottom] = dirichlet(0);
 
 /* The so-called "wetting condition", which is the fraction of fluid enforced by VoF method. */
-f[bottom] = 0.0;
+//f[bottom] = 0.0;
+f[bottom] = neumann(0);
 
 
 /* Contact angle specification (removed axial symmetry) */
