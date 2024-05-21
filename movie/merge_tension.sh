29726e6c31e349d74261372ff86cfe994369b0d4
Matthew Cho <mc462@login02.tartan.njit.edu>
2024-05-20 20:26:10 -0400

    cleanup

--- movie/merge_tension.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-module purge > /dev/null 2>&1
-module load easybuild
-module load GCCcore/12.2.0
-module load FFmpeg
-
-ffmpeg -i tension_test_15.mp4 -i tension_test_100.mp4 -filter_complex hstack compare_15_100.mp4
-
--- run_exp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-vel=$1
-sbatch --error="slurm_out/drop-$vel.err" --output="slurm_out/drop-$vel.out" vel_exp.sh $vel
-
